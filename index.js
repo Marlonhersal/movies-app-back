b1f98ab53b2e306fe052accfa75b4e9b1ca63f52
MarlonHersal <hern.sal.dz@gmail.com>
2022-11-28 16:04:08 -0600

    Port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Express
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT|| 3000;
 //Routing
 const routerApi = require('./routes')
 //Importando Middlewares
