fbe16eec831501c085c7f6293806f943b1b5e286
MarlonHersal <hern.sal.dz@gmail.com>
2022-11-28 18:54:09 -0600

    Deploy

--- index.js
+++ index.js
@@ -5,87 +5,16 @@ const port = process.env.PORT|| 3000;
 //Routing
 const routerApi = require('./routes')
 //Importando Middlewares
-const {errorHandler,boomErrorHandler} = require('./middlewares/error.handler');
+const {errorHandler,boomErrorHandler} = require('./middlewares/error.handler')
 
 
 //Convirtiendo las repuestas en JSON
 app.use(express.json());
 
 
-/* app.get('/', (req, res)=>{
+app.get('/', (req, res)=>{
     res.send('API REST DE PELÍCULAS')
 })
- */
-
-const UsersService = require('./services/users.service');
-const service = new UsersService();
-
-//Validador de schemas
-const validatorHandler = require('./middlewares/validator.handler');
-const {getUserSchema,createUserSchema,updateUserSchema} = require('./schemas/user.schema');
-
-
-app.get('/users', async (req, res, next)=>{
-    try{
-        const response = await service.find();
-        res.json(response)
-    }
-    catch(err){
-        next(err)
-    }
-});
-
-app.get('/:id',
-    validatorHandler(getUserSchema, 'params')
-    ,async (req, res, next)=>{
-    try{
-        const response = await service.findOne(req.params.id);
-        res.json(response)
-    }
-    catch(err){
-        next(err)
-    }
-});
-
-app.post('/',
-    validatorHandler(createUserSchema, 'body')
-    ,async (req, res, next)=>{
-    try{
-        const response = await service.create(req.body);
-        res.json(response)
-    }
-    catch(err){
-        next(err)
-    }
-});
-
-app.patch('/:id',
-    validatorHandler(getUserSchema, 'params'),
-    validatorHandler(updateUserSchema, 'body')
-    ,async (req, res, next)=>{
-    try{
-        const response = await service.update(req.params.id,req.body);
-        res.json(response)
-    }
-    catch(err){
-        next(err)
-    }
-});
-
-app.delete('/:id', 
-    validatorHandler(getUserSchema, 'params')
-    ,async (req, res, next)=>{
-    try{
-        const response = await service.delete(req.params.id);
-        res.json(response)
-    }
-    catch(err){
-        next(err)
-    }
-});
-
-
-
 
 //Conectando con el Routing
 routerApi(app)
