6a3679aea596642cec8daff0f6a2dea1a4ab73e2
MarlonHersal <hern.sal.dz@gmail.com>
2022-11-28 18:57:40 -0600

    Deploy 2

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const actorsRouter = require('./actors.router')
 
 function routerApi(app){
     const router = express.Router();
-    app.use('/', router)
+    app.use('/movies', router)
 
     //Definiendo rutas
     router.use('/users', usersRouter)
