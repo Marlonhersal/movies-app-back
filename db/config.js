9314fe05065315096aed26e0ecb766cf8e3c59ce
MarlonHersal <hern.sal.dz@gmail.com>
2022-12-05 19:17:30 -0600

    Config

--- db/config.js
+++ db/config.js
@@ -13,6 +13,9 @@ module.exports = {
     },
     development: {
         url: URI,
-        dialect: 'postgres'
+        dialect: 'postgres',
+        ssl : {
+            rejectUnauthorized:  false
+        }
     }
 }
