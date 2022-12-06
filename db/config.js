c875ffe132dbc28ce102fe6a9f917ec471ed30a8
MarlonHersal <hern.sal.dz@gmail.com>
2022-12-05 19:10:29 -0600

    Config

--- db/config.js
+++ db/config.js
@@ -2,11 +2,9 @@ const {config}= require('../config/config')
 
 const URI = config.dbUrl;
 
+
 module.exports = {
-    development: {
-        url: URI,
-        dialect: 'postgres'
-    },
+
     production: {
         url: URI,
         dialect: 'postgres',
