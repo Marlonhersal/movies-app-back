ab1e81c821649e549b52844c406c6b295abe32d5
MarlonHersal <hern.sal.dz@gmail.com>
2022-12-05 19:27:38 -0600

    ssl

--- db/config.js
+++ db/config.js
@@ -14,8 +14,8 @@ module.exports = {
     development: {
         url: URI,
         dialect: 'postgres',
-        ssl : {
+        SSL : {
             rejectUnauthorized:  false
-        }
+        },
     }
 }
